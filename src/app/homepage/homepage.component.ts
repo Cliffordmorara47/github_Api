be916d7f777599a3b0c3ca2ed27c1e8b0c6abd58
cliffordmorara <cliffordmorara45@gmail.com>
2021-02-07 16:01:46 +0300

    generated a homepage component

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    AOS.init();
+  }
+
+}
