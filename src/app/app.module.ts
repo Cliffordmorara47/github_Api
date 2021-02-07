3f6e0ccd02dde4a0e961c7504259552d52ffa43f
cliffordmorara <cliffordmorara45@gmail.com>
2021-02-07 16:00:16 +0300

    Initiated animate on scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GithubComponent } from './github/github.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    GithubComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
