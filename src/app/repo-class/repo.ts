db1003fe147945a61a1438bd518cf722e4813ffe
cliffordmorara <cliffordmorara45@gmail.com>
2021-02-09 12:33:14 +0300

    Working on the search functionality

---
+++ src/app/repo-class/repo.ts
@@ -0,0 +1,10 @@
+export class Repo {
+    constructor(
+        public name: string,
+        public full_name: string,
+        public description: string,
+        public created_at: string,
+        public updated_at: string,
+        public pushed_at: string)
+         { }
+}
